e960180d496e40f4fd8dd28a940efcd368ee22cc
Patrick Sanders <psanders@pindrop.com>
2016-06-23 17:05:12 -0400

    make bipol less synchronous

--- bipol.py
+++ bipol.py
@@ -1,8 +1,12 @@
-from helga.plugins import command
+from helga.plugins import command, ResponseNotReady
 from requests import get
+from twisted.internet import reactor
+
+def bipol_async(client, channel, args):
+    response = get('http://bipolasaservice.herokuapp.com/bipol')
+    client.msg(channel, response.text)
 
 @command('bipol', help='listen to what he has to say')
-def bipol(client, channel, nick, message, cmd, args):
-    response = get('http://bipolasaservice.herokuapp.com/bipol')
-    return response.text
+    reactor.callLater(5, bipol_async, client, channel, args)
+    raise ResponseNotReady
 
