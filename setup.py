0ee101898ccd66764b5152aaa70959c2b25a137a
Patrick Sanders <psanders@pindrop.com>
2016-06-24 10:04:05 -0400

    fix manifest

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='bipol',
-    version='0.0.2',
+    version='0.0.3',
     description='A bipol plugin',
     author='Patrick Sanders',
     author_email='mail@patricksanders.net',
+    url='https://github.com/patricksanders/bipol',
     install_requires=[
         'requests',
         'helga',
