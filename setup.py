5824a4bcd70ab4beb789b352d95130477913b63b
Patrick Sanders <psanders@pindrop.com>
2016-06-24 09:51:48 -0400

    add install requirements

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='bipol',
-    version='0.0.1',
+    version='0.0.2',
     description='A bipol plugin',
-    author="Patrick Sanders"
-    author_email="mail@patricksanders.net",
+    author='Patrick Sanders',
+    author_email='mail@patricksanders.net',
+    install_requires=[
+        'requests',
+        'helga',
+    ],
     packages=find_packages(),
     py_modules=['bipol'],
     include_package_data=True,
